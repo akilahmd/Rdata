bffaf43f8a4bf7830ca483df95417e90a7f58f3d
akilahmd <aqe_85@hotmail.com>
2016-10-21 17:13:58 +0200

    final changes

--- R/predictionsofBos.R
+++ R/predictionsofBos.R
@@ -1,5 +1,6 @@
 library(caret)
 library(mlbench)
+library(Regression)
 data(BostonHousing)
 set.seed(506)
 
@@ -17,6 +18,13 @@ prediction1<- predict(model1,val_data)  #prediction of the medv on the unseen da
 plot1<-plot(model1)                     #plot of the model in the training.
 plot2<-plot(prediction1)                #plot of prediction using model1.
 
+#fitting a model using our function with lambda =0 and using the bostonhousing data
+
+our_model<-ridgereg(medv~.,data = tra_data,lambda = 0)
+our_predic1<- our_model$rid_fitted
+print(our_predic1)
+
+
 #10 fold cross validation
 train_control <- trainControl(method="cv", number=10)
 model2 <- train(medv ~ ., data = tra_data, trControl=train_control, method = "leapForward")
@@ -25,3 +33,14 @@ prediction2 <- predict(model2,val_data )
 plot(prediction2)
  
  
+
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 10)
+
+fit_ridgereg <- train(medv~ .,data= tra_data, method="ridge", trControl = fitControl,
+                  tuneGrid= expand.grid(lambda=seq(0,8,.5)) ) #http://finzi.psych.upenn.edu/library/AppliedPredictiveModeling/chapters/06_Linear_Regression.R
+
+print(fit_ridgereg)
+ggplot(fit_ridgereg)
